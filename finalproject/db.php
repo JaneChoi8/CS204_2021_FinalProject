b7a3c5ca5c6d064c494aa956d0d7426d1ed53034
janechoi8 <janechoi2001@gmail.com>
2021-06-16 13:53:53 +0700

    landing page

---
+++ finalproject/db.php
@@ -0,0 +1,8 @@
+<?php
+  $host = "localhost";
+  $user = "root";
+  $pw = "";
+  $db = "finalproject";
+
+  $conn = new MYSQLI($host, $user, $pw, $db);
+ ?>
---
+++ finalproject/index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title></title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+    <!-- fixed-top | sticky-top | fixed-bottom -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div class="container ">
+            <a class="navbar-brand " href="#">Freelance</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav float-right">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Sign in <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Join</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  </header>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container wrapper ">
+      <h1 class="display-3">Find your way</h1>
+      <p class="lead">Let take a look about our service</p>
+      <form class="form-inline my-2 my-lg-0 mb-3">
+          <input class="form-control mr-sm-3" type="search" placeholder="Search the  recruitment" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+  <div class="container ">
+    <div class="row">
+      <div class="col-md-4">
+        <h3>24/7 Support</h3><br>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+      <div class="col-md-4">
+        <h3>Best for every members</h3><br>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+      <div class="col-md-4">
+        <h3>Safe</h3><br>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      </div>
+    </div>
+  </div>
+  <h2 class="mt-3 mb-4 ml-3">Explore our service</h2>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-3">
+        <i class="fas fa-paint-brush"></i>
+        <hr>
+        <h5>Graphic design</h5>
+      </div>
+      <div class="col-md-3">
+        <i class="fas fa-desktop"></i>
+        <hr>
+        <h5>Web design</h5>
+      </div>
+      <div class="col-md-3">
+        <i class="fas fa-photo-video"></i>
+        <hr>
+        <h5>Video Making</h5>
+      </div>
+      <div class="col-md-3">
+        <i class="fas fa-hand-holding-usd"></i>
+        <hr>
+        <h5>Business</h5>
+      </div>
+    </div>
+  </div>
+  <footer class="bg-dark text-center text-white mt-5">Copyright:</footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
