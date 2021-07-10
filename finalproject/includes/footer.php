b6720df0d922aef0098a3ad1fc7ecb572043b994
janechoi8 <janechoi2001@gmail.com>
2021-07-10 12:26:30 +0700

    update

--- finalproject/includes/footer.php
+++ finalproject/includes/footer.php
@@ -1,15 +1,31 @@
 <div class="pre-footer row">
   <div class="col-md-3 ">
     <h4>Customer Care</h4>
-    <a href="#">Contact us</a>
+    <ul>
+      <li>Contact US</li>
+      <li>Order information</li>
+      <li>FAQ</li>
+      <li>Sitemap</li>
+    </ul>
   </div>
   <div class="col-md-3">
     <h4>Our company</h4>
-    <a href="#">Contact us</a>
+    <ul>
+      <li>Find a Boutique</li>
+      <li>Careers</li>
+      <li>Sustainability</li>
+      <li>Website Policies</li>
+      <li>Report an Issue</li>
+      <li>Not Sell My Info</li>
+    </ul>
   </div>
   <div class="col-md-3">
     <h4>Legal Area</h4>
-    <a href="#">Contact us</a>
+    <ul>
+      <li>Terms of use</li>
+      <li>Privacy notice</li>
+      <li>Conditions of sale</li>
+    </ul>
   </div>
   <div class="col-md-3">
     <h4>Follow us</h4>
@@ -18,7 +34,7 @@
     <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
   </div>
 </div>
-<footer class="bg-light text-center ">Copyright: </footer>
+<footer class="bg-light text-center ">Copyright: ©  Jewellery.2021 </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
